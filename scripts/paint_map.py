60a60d9ace97678337792349e5175542c5f52fd1
MountAye <45800208+MountAye@users.noreply.github.com>
2023-06-25 17:49:36 -0500

    Decoration: add names on the map

--- scripts/paint_map.py
+++ scripts/paint_map.py
@@ -1,7 +1,8 @@
 import json
+import cv2
 import numpy as np
 import pandas as pd
-from skimage import io,segmentation,util
+from skimage import io,segmentation,morphology,measure,util
 
 def hex2color(hex:str):
     assert hex[0] == "#", "ColorError: lost `#` before hex code."
@@ -22,11 +23,52 @@ latest = np.zeros((*base.shape,3),dtype=int)
 latest[mask_lands] = hex2color(config["color_land"])
 latest[mask_ocean] = hex2color(config["color_seas"])
 
-for i,series in registered.iterrows():
-    row = int(series["PIN_ROW"])
-    col = int(series["PIN_COL"])
-    mask_territory = segmentation.flood(borders,(row,col),connectivity=1)
-    latest[np.logical_and(mask_lands,mask_territory)] = hex2color(series["COLOR"])
+for name in registered["STATE"].unique():
+    count_words = len(name.split(" "))
+    entries = registered.loc[registered["STATE"].eq(name)]
+    nation_territories = np.zeros_like(base,dtype=bool)
+    name_color = np.array([255,255,255])
+    nation_color = hex2color(config["color_land"])
+    for i,series in entries.iterrows():
+        row = int(series["PIN_ROW"])
+        col = int(series["PIN_COL"])
+        nation_territory = segmentation.flood(borders,(row,col),connectivity=1)
+        nation_territories = np.logical_or(nation_territories,nation_territory)
+        nation_land = np.logical_and(mask_lands,nation_territory)
+        
+        nation_color = hex2color(series["COLOR"])
+        latest[nation_land] = nation_color
+        if np.dot(nation_color,np.array([0.299,0.587,0.114]))>256/2:
+            name_color = np.array([0,0,0])
+    # Because we don't know if their territories are connected:
+    nation_territories = morphology.binary_opening(nation_territories,footprint=np.ones((3,3)))
+    for prop_territory in measure.regionprops(measure.label(nation_territories)):
+        angle = prop_territory.orientation * 180 / np.pi - 90
+        if angle < -90:
+            angle = 180 + angle
+        y,x = prop_territory.centroid
+        x -= 0.35 * prop_territory.axis_major_length * np.cos(angle/180*np.pi)
+        x = int(x)
+        y += 0.35 * prop_territory.axis_major_length * np.sin(angle/180*np.pi)
+        y = int(y)
+
+        (name_width,name_height),_ = cv2.getTextSize(name,fontFace=cv2.FONT_HERSHEY_TRIPLEX,fontScale=1,thickness=1)
+        scale = 0.8 * prop_territory.axis_major_length / name_width
+
+        text = cv2.putText(
+                           img=np.zeros_like(base,dtype=np.uint8), # Image.
+                           text=name,	               # Text string to be drawn.
+                           org=(x,y),	                   # Bottom-left corner of the text string in the image.
+                           fontFace=cv2.FONT_HERSHEY_TRIPLEX, # Font type, see HersheyFonts.
+                           fontScale=scale,	                   # Font scale factor that is multiplied by the font-specific base size.
+                           color=255,	                       # Text color.
+                           thickness=2,	                   # Thickness of the lines used to draw a text.
+                           lineType=cv2.LINE_AA,	           # Line type. See LineTypes
+                           bottomLeftOrigin=False,	           # When true, the image data origin is at the bottom-left corner. Otherwise, it is at the top-left corner.
+                          )
+        M = cv2.getRotationMatrix2D((x,y), angle, 1)
+        text = cv2.warpAffine(text, M, (text.shape[1], text.shape[0]))
+        latest[(text>255/2)] = name_color
 
 latest[(borders>0)] = np.array([255,255,255])
 io.imsave("latest.png",util.img_as_ubyte(latest))
