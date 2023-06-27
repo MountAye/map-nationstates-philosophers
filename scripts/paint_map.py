c41a4ebf7341d73e3bfb4b504cd87f753edd3a2d
MountAye <45800208+MountAye@users.noreply.github.com>
2023-06-27 09:47:32 -0500

    tagging function good enough; add Arcanterra

--- scripts/paint_map.py
+++ scripts/paint_map.py
@@ -42,33 +42,48 @@ for name in registered["STATE"].unique():
             name_color = np.array([0,0,0])
     # Because we don't know if their territories are connected:
     nation_territories = morphology.binary_opening(nation_territories,footprint=np.ones((3,3)))
-    for prop_territory in measure.regionprops(measure.label(nation_territories)):
-        angle = prop_territory.orientation * 180 / np.pi - 90
+    nation_territories_label = measure.label(nation_territories)
+    for prop_territory_all in measure.regionprops(nation_territories_label):
+        prop_territory_land = measure.regionprops(util.img_as_ubyte(np.logical_and(mask_lands,(nation_territories_label==prop_territory_all.label))))[0]
+        angle = prop_territory_land.orientation * 180 / np.pi - 90
         if angle < -90:
             angle = 180 + angle
-        y,x = prop_territory.centroid
-        x -= 0.35 * prop_territory.axis_major_length * np.cos(angle/180*np.pi)
+
+
+        words = name.split("\ ")
+        n_words = len(words)
+        longest = sorted(words,key=lambda x:len(x),reverse=True)[0]
+
+        (name_width,name_height),_ = cv2.getTextSize(longest,fontFace=cv2.FONT_HERSHEY_TRIPLEX,fontScale=1,thickness=1)
+        scale_x = 0.8 * prop_territory_land.axis_major_length / name_width
+        scale_y = 0.8 * prop_territory_land.axis_minor_length / name_height / n_words
+        scale = min([scale_x,scale_y])
+
+        y,x = prop_territory_land.centroid
+        x -= 0.45 * name_width * scale * np.cos(angle/180*np.pi)
+        y += 0.45 * name_width * scale * np.sin(angle/180*np.pi)
         x = int(x)
-        y += 0.35 * prop_territory.axis_major_length * np.sin(angle/180*np.pi)
         y = int(y)
 
-        (name_width,name_height),_ = cv2.getTextSize(name,fontFace=cv2.FONT_HERSHEY_TRIPLEX,fontScale=1,thickness=1)
-        scale = 0.8 * prop_territory.axis_major_length / name_width
-
-        text = cv2.putText(
-                           img=np.zeros_like(base,dtype=np.uint8), # Image.
-                           text=name,	               # Text string to be drawn.
-                           org=(x,y),	                   # Bottom-left corner of the text string in the image.
-                           fontFace=cv2.FONT_HERSHEY_TRIPLEX, # Font type, see HersheyFonts.
-                           fontScale=scale,	                   # Font scale factor that is multiplied by the font-specific base size.
-                           color=255,	                       # Text color.
-                           thickness=2,	                   # Thickness of the lines used to draw a text.
-                           lineType=cv2.LINE_AA,	           # Line type. See LineTypes
-                           bottomLeftOrigin=False,	           # When true, the image data origin is at the bottom-left corner. Otherwise, it is at the top-left corner.
-                          )
-        M = cv2.getRotationMatrix2D((x,y), angle, 1)
-        text = cv2.warpAffine(text, M, (text.shape[1], text.shape[0]))
-        latest[(text>255/2)] = name_color
+        for w,word in enumerate(words):
+            xw = int(x + scale * name_height * 1.2 * (w - (n_words-1)/2) * np.sin(angle/180*np.pi)) 
+            yw = int(y + scale * name_height * 1.2 * (w - (n_words-1)/2) * np.cos(angle/180*np.pi)) 
+            
+            text = cv2.putText(
+                               img=np.zeros_like(base,dtype=np.uint8), # Image.
+                               text=word,	                           # Text string to be drawn.
+                               org=(xw,yw),	                           # Bottom-left corner of the text string in the image.
+                               fontFace=cv2.FONT_HERSHEY_TRIPLEX,      # Font type, see HersheyFonts.
+                               fontScale=scale,	                       # Font scale factor that is multiplied by the font-specific base size.
+                               color=255,	                           # Text color.
+                               thickness=2,	                           # Thickness of the lines used to draw a text.
+                               lineType=cv2.LINE_AA,	               # Line type. See LineTypes
+                               bottomLeftOrigin=False,	               # When true, the image data origin is at the bottom-left corner. Otherwise, it is at the top-left corner.
+                              )
+            M = cv2.getRotationMatrix2D((xw,yw), angle, 1)
+            text = cv2.warpAffine(text, M, (text.shape[1], text.shape[0]))
+            latest[(text>255/2)] = name_color
+    print(f"FINISHED PAINTING: {name}")
 
 latest[(borders>0)] = np.array([255,255,255])
 io.imsave("latest.png",util.img_as_ubyte(latest))
